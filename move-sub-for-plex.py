f0f53521eebb4d59547c482854b41a5b3c34961b
fishoux <fischer.stephane@gmail.com>
2022-10-11 23:00:13 +0200

    arg variable

---
+++ move-sub-for-plex.py
@@ -0,0 +1,41 @@
+# shared by https://www.reddit.com/user/Saafris/ here https://pastebin.com/pbpJU08R
+# TODO Search for all Movie/TV Show folders
+# TODO Apply this script on all Movie/TV Show folders
+# TODO Remove all RARBG.txt files
+
+from os import walk
+import shutil
+import os
+import sys
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv[1]))
+
+path = os.path.abspath(sys.argv[1])
+subsPath = path + "\Subs"
+
+print(path)
+print(subsPath)
+
+def getFilesize(path, filename):
+    full = path + "\\" + filename
+    size = os.path.getsize(full)
+    return size
+    
+fileDict = {}
+for (dirpath, dirnames, filenames) in walk(subsPath):
+    for (file) in filenames:
+        mylang = file.split("_")[-1]
+        sub = file.split("_")[0]
+ 
+        newName = dirpath.split("\\")[-1]
+        oldPath = dirpath + "\\" + file
+        if (mylang == 'English.srt'):
+        
+            fullPath = path + "\\" + newName + "." + "en.srt"
+            currentLargest = fileDict.get(fullPath, -1)
+            thisSize = getFilesize(dirpath, file)
+            
+            if (currentLargest <= thisSize):
+                print(oldPath)
+                print("\t\t" + fullPath)
+                shutil.copy(oldPath, fullPath)
